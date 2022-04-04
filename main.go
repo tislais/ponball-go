f979586fa06383e52dc3b8424316470f1f1bacc4
Tis Lais <tistististis@gmail.com>
2022-04-04 08:54:49 -0700

    gorbage

--- main.go
+++ main.go
@@ -4,21 +4,47 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 
 	"golang.org/x/net/html"
 )
 
-func main() {
-	fmt.Println("banan")
+type machine struct {
+	title string
+	ipdbId int
+	manufacturer string
+}
 
-	res, err := http.Get("https://www.ipdb.org/machine.cgi?id=20")
+func main() {
+	
+	fetchedHTML, err := fetchHTML("https://www.ipdb.org/machine.cgi?id=20")
 	if err != nil {
-		fmt.Printf("%s", errors.New(err.Error()))
+		fmt.Println("ouchies")
+	}
+	defer fetchedHTML.Close()
+	
+	
+	// m, err := processTokens(fetchedHTML)
+	// if err != nil {
+	// 	fmt.Printf("%v\n", err)
+	// }
+
+	m2, err := parseHTML(fetchedHTML)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	defer res.Body.Close()
+	fmt.Printf("Title: %v\n", m2.title)
+}
+
+func fetchHTML(URL string) (io.ReadCloser, error) {
+	res, err := http.Get(URL)
+	if err != nil {
+		fmt.Printf("hi %s", errors.New(err.Error()))
+	}
 
 	if res.StatusCode != http.StatusOK {
 		fmt.Printf("Response status code was %d\n", res.StatusCode)
@@ -26,30 +52,73 @@ func main() {
 
 	ct := res.Header.Get("Content-Type")
 	if !strings.HasPrefix(ct, "text/html") {
-		fmt.Printf("Response content type was %s, not text/html\n", ct)
+		err = errors.New("big bad: response content was not text/html, instead got " + ct)
 	}
+	return res.Body, err
+}
 
-	tokenizer := html.NewTokenizer(res.Body)
+func parseHTML(body io.ReadCloser) (machine, error) {
+	s, err := ioutil.ReadAll(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Printf("%s", s)
+
+	text := string(s)
+
+	dom, err := html.Parse(strings.NewReader(text))
+	fmt.Println("dom ", dom.LastChild)
+
+	return machine{}, err
+}
+
+func processTokens(body io.ReadCloser) (machine, error) {
+	// fmt.Println(dom, err)
+	// if err != nil {
+	// 	fmt.Printf("Error from html.Parse:%v", err)
+	// }
+	// var f func(*html.Node)
+	// f = func(n *html.Node) {
+	// 	if n.Type == html.ElementNode && n.Data == "a" {
+	// 		for _, a := range n.Attr {
+	// 			if a.Key == "href" {
+	// 				fmt.Println(a.Val)
+	// 				break
+	// 			}
+	// 		}
+	// 		for c := n.FirstChild; c != nil; c = c.NextSibling {
+	// 			f(c)
+	// 		}
+	// 	}
+	// 	f(dom)
+	// }
+	// z = tokenizer
+	var err error
+	z := html.NewTokenizer(body)
+	m := machine{}
 	
 	for {
-		tokenType := tokenizer.Next()
-		if tokenType == html.ErrorToken {
-			err := tokenizer.Err()
+		// tt = tokenType
+		tt := z.Next()
+		if tt == html.ErrorToken {
+			err := z.Err()
 			if err == io.EOF {
+				err = nil
 				break
 			}
-			fmt.Println(tokenizer.Err())
+			fmt.Println(z.Err())
 		}
 
-		if tokenType == html.StartTagToken {
-			token := tokenizer.Token()
+		if tt == html.StartTagToken {
+			token := z.Token()
 			if token.Data == "title" {
-				tokenType = tokenizer.Next()
-				if tokenType == html.TextToken {
-					fmt.Println(tokenizer.Token().Data)
-					break
+				tt = z.Next()
+				if tt == html.TextToken {
+					m.title = z.Token().Data
+					fmt.Println(z.Token().Data)
 				}
-			}
+			} 
 		}
 	}
+	return m, err
 }
